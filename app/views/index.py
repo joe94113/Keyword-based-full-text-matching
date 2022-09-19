b4f1154fb8280e5b12ad67e33c6b8ac7126ed0d5
joe <joe94113@gmail.com>
2022-09-19 15:34:17 +0800

    feat: split string to sentences and ignore name

--- app/views/index.py
+++ app/views/index.py
@@ -66,7 +66,7 @@ class Views:
                     checkInJson = False
 
             for d in xmlFile:
-                sentences = Views.textToSentence(d['description'])
+                sentences = Views.split_into_sentences(d['description'])
                 # print(sentences)
                 for sentence in sentences:
                     originalText = [x.replace(' ', '') for x in sentence.split(' ')]
@@ -129,4 +129,40 @@ class Views:
         non_fragments = re.compile(r'$|\d+($|:\d+.*\d+.*$)')
         full_text = "".join([line for line in text if not non_fragments.match(line)])
         sentences = full_text.split('. ')
+        return sentences
+    
+    def split_into_sentences(text):
+        alphabets= "([A-Za-z])"
+        prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+        suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+        starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+        acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+        websites = "[.](com|net|org|io|gov)"
+        digits = "([0-9])"
+
+        text = " " + text + "  "
+        text = text.replace("\n"," ")
+        text = re.sub(prefixes,"\\1<prd>",text)
+        text = re.sub(websites,"<prd>\\1",text)
+        text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+        if "..." in text: text = text.replace("...","<prd><prd><prd>")
+        if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+        text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+        text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+        text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+        text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+        text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+        text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+        text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+        if "”" in text: text = text.replace(".”","”.")
+        if "\"" in text: text = text.replace(".\"","\".")
+        if "!" in text: text = text.replace("!\"","\"!")
+        if "?" in text: text = text.replace("?\"","\"?")
+        text = text.replace(".",".<stop>")
+        text = text.replace("?","?<stop>")
+        text = text.replace("!","!<stop>")
+        text = text.replace("<prd>",".")
+        sentences = text.split("<stop>")
+        sentences = sentences[:-1]
+        sentences = [s.strip() for s in sentences]
         return sentences
